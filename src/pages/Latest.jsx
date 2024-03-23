398541a02ff0e273c9a8a0dff8cbf3298be4de99
Shivdutt chauhan <shivduttkumar9161@gmail.com>
2024-03-23 08:24:29 +0530

    Basic changes

--- src/pages/Latest.jsx
+++ src/pages/Latest.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
-import Footer from "../components/Footer/Footer"
+import React, { useEffect, useState } from 'react'
+
 
 const Latest = () => {
+  const [data, setData] = useState([])
+
+  useEffect(()=> {
+    getData()
+  },[])
+
+  const getData = () => {
+    fetch('data.json', {headers : {
+      'Content-Type':'application/json',
+      'Accept': 'application/json'
+    }}).then((response)=> {
+      return response.json()
+    }).then((myJson) => {
+      // console.log(data, "data")
+      setData(myJson)
+    })
+  }
+
   return (
     <>
-      this is latest
-      <Footer />
+     <p>List movies</p>
+     {
+        data.map((item)=> {
+          return(
+            <div className="card" style={{ width: '16rem', display: "inline-block" }}>
+            <img src={item.image_url} style={{width: '100%'}} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{item.title}</h5>
+            </div>
+          </div>
+          )
+        })
+      }
     </>
   )
 }
