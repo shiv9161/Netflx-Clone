a4a5d80727aebad9de18f4636ae0cc2ad02161f5
Shivdutt chauhan <shivduttkumar9161@gmail.com>
2024-04-19 01:28:59 +0530

    adding more movies to latest page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,52 +1,79 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "./style.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 // import Netflix from "../../static/Netflix-logo-red-black-png.png"
-import Netflix from "../../static/netflix-logo-png-large.png"
+import Netflix from "../../static/netflix-logo-png-large.png";
 // import { IoSearchSharp } from "react-icons/io5";
-
 
 const Header = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light nav-back">
-                <div className="container">
-                        <img src={Netflix} className='logo-img' alt='logo'/>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item header-line" style={{marginRight: "10px"}}>
-                                <Link className="nav-link active text-white" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item header-line" style={{marginRight: "10px"}}>
-                                <Link className="nav-link active text-white" to="/tvshows">TV Shows</Link>
-                            </li>
-                            <li className="nav-item header-line" style={{marginRight: "10px"}}>
-                                <Link className="nav-link active text-white" to="/movies">Movies</Link>
-                            </li>
-                            <li className="nav-item header-line" style={{marginRight: "10px"}}>
-                                <Link className="nav-link active text-white" to="/latest">Latest</Link>
-                            </li>
-                            {/* Add more items as needed */}
-                        </ul>
-                    </div>
-                    <div>
-                      <input type="text" className="form-control mr-sm-2" id="searchInput" placeholder="Search"/>                    
-                    </div>
-                </div>
-            </nav>
+        <div className="container">
+          <img src={Netflix} className="logo-img" alt="logo" />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li
+                className="nav-item header-line"
+                style={{ marginRight: "10px" }}
+              >
+                <Link className="nav-link active text-white" to="/">
+                  Home
+                </Link>
+              </li>
+              <li
+                className="nav-item header-line"
+                style={{ marginRight: "10px" }}
+              >
+                <Link className="nav-link active text-white" to="/latest">
+                  Latest
+                </Link>
+              </li>
+              <li
+                className="nav-item header-line"
+                style={{ marginRight: "10px" }}
+              >
+                <Link className="nav-link active text-white" to="/tvshows">
+                  TV Shows
+                </Link>
+              </li>
+              <li
+                className="nav-item header-line"
+                style={{ marginRight: "10px" }}
+              >
+                <Link className="nav-link active text-white" to="/movies">
+                  Movies
+                </Link>
+              </li>
+              {/* Add more items as needed */}
+            </ul>
+          </div>
+          <div>
+            <input
+              type="text"
+              className="form-control mr-sm-2"
+              id="searchInput"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Latest.jsx
+++ src/pages/Latest.jsx
@@ -1,42 +1,53 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from "react";
 
 const Latest = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
-  useEffect(()=> {
-    getData()
-  },[])
+  useEffect(() => {
+    getData();
+  }, []);
 
   const getData = () => {
-    fetch('data.json', {headers : {
-      'Content-Type':'application/json',
-      'Accept': 'application/json'
-    }}).then((response)=> {
-      return response.json()
-    }).then((myJson) => {
-      // console.log(data, "data")
-      setData(myJson)
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
-  }
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        // console.log(data, "data")
+        setData(myJson);
+      });
+  };
 
   return (
     <>
-     <p>List movies</p>
-     {
-        data.map((item)=> {
-          return(
-            <div className="card" style={{ width: '16rem', display: "inline-block" }}>
-            <img src={item.image_url} style={{width: '100%'}} className="card-img-top" alt="..." />
+      <p className="h2">
+        <b>List Movies</b>
+      </p>
+      {data.map((item) => {
+        return (
+          <div
+            className="card"
+            style={{ width: "10rem", display: "inline-block" }}
+          >
+            <img
+              src={item.image_url}
+              style={{ width: "100%" }}
+              className="card-img-top img-fluid"
+              alt="..."
+            />
             <div className="card-body">
               <h5 className="card-title">{item.title}</h5>
             </div>
           </div>
-          )
-        })
-      }
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-export default Latest
+export default Latest;
