c5687a2f361acd8d9be19feaf3d0ce8473c7c601
Shivdutt chauhan <shivduttkumar9161@gmail.com>
2024-04-24 15:12:20 +0530

    all done

--- src/components/HomeCraousel/HomeCraousel.jsx
+++ src/components/HomeCraousel/HomeCraousel.jsx
@@ -1,45 +1,11 @@
 import React from "react";
-import Wallpaper2 from "../../static/wallpaper1.jpg";
-import Logo from "../../static/godlogo.png";
+import Wallpaper2 from "../../static/wallpaper1.png";
 import "./style.css";
-import { FaPlay } from "react-icons/fa";
-import { HiOutlinePlus } from "react-icons/hi";
+
 const HomeCraousel = () => {
   return (
     <div>
       <img className="img-fluid" src={Wallpaper2} alt="" />
-      <div className="container">
-        {/* <div>
-          <img className="img-fluid poster-logo" src={Logo} alt="poster logo" />
-        </div> */}
-
-        {/* <div>
-          <p className="poster-para">2020 | 16+ | 1h 55min | Action</p>
-          <p className="poster-line col-4">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            consequuntur nemo dicta itaque veritatis perferendis, perspiciatis
-            porro deserunt dignissimos numquam. Harum in, deserunt autem
-            pariatur optio sed voluptatem commodi. Ipsum cum molestias, aperiam!
-          </p>
-        </div> */}
-
-        {/* <div className="poster-btn">
-          <button className="btn btn-danger" style={{ marginRight: "10px" }}>
-            <FaPlay /> PLAY
-          </button>
-          <button className="btn btn-primary">
-            <HiOutlinePlus /> MY LIST
-          </button>
-        </div>
-
-        <div className="poster-trailer">
-          <p className="">
-            <FaPlay />
-            <b> WATCH TRAILER</b>
-          </p>
-        </div> */}
-      </div>
-      {/* <Footer /> */}
     </div>
   );
 };
--- src/components/cardSlider/CardSlider.jsx
+++ src/components/cardSlider/CardSlider.jsx
@@ -15,13 +15,12 @@ const CardGrid = () => {
         Accept: "application/json",
       },
     })
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((myJson) => {
         setData(myJson);
       });
   };
+
   return (
     <div className="container">
       <h2 className="text-center m-3">
@@ -32,11 +31,15 @@ const CardGrid = () => {
       <div className="row row-cols-1 row-cols-md-3 g-4">
         {data.map((item) => (
           <div className="col" key={item.id}>
-            <div className="card">
+            <div className="card card-hover">
+              {" "}
+              {/* Added card-hover class */}
               <img src={item.image} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{item.title}</h5>
-                <p className="card-text">{item.para}</p>
+                <p className="card-text" style={{ textAlign: "justify" }}>
+                  {item.para}
+                </p>
               </div>
             </div>
           </div>
